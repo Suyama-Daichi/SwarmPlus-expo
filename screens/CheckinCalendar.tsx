2385129d3a9cc77ab05b339351dbd9d03cb26231
daichi.suyama <symdit@gmail.com>
2021-03-13 20:01:48 +0900

    add: yarn add react-native-calendars

--- screens/CheckinCalendar.tsx
+++ screens/CheckinCalendar.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { Calendar, CalendarList, LocaleConfig, Agenda } from 'react-native-calendars'
 
 import EditScreenInfo from '../components/EditScreenInfo'
 import { Text, View } from '../components/Themed'
@@ -13,19 +14,36 @@ export default function CheckinCalander() {
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-})
+LocaleConfig.locales.jp = {
+  monthNames: [
+    '1月',
+    '2月',
+    '3月',
+    '4月',
+    '5月',
+    '6月',
+    '7月',
+    '8月',
+    '9月',
+    '10月',
+    '11月',
+    '12月',
+  ],
+  monthNamesShort: [
+    '1月',
+    '2月',
+    '3月',
+    '4月',
+    '5月',
+    '6月',
+    '7月',
+    '8月',
+    '9月',
+    '10月',
+    '11月',
+    '12月',
+  ],
+  dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+  dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+}
+LocaleConfig.defaultLocale = 'jp'
