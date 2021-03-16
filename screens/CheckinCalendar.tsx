809554ec724eaa970cf821f094775d958b6fee6e
daichi.suyama <symdit@gmail.com>
2021-03-16 22:30:42 +0900

    feature: agendaコンポーネントを表示

--- screens/CheckinCalendar.tsx
+++ screens/CheckinCalendar.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Calendar, CalendarList, LocaleConfig, Agenda } from 'react-native-calendars'
-
-import EditScreenInfo from '../components/EditScreenInfo'
-import { Text, View } from '../components/Themed'
+import { View } from 'react-native'
+import { LocaleConfig, Agenda } from 'react-native-calendars'
 
 export default function CheckinCalander() {
+  /**
+   * 月ごとのチェックインを取得する
+   * @param month date-string
+   */
+  const fetchCheckinForMonth = (month: string) => {}
+  /**
+   * 日ごとのチェックインを取得する
+   * @param day date-string
+   */
+  const fetchCheckinForDay = (day: string) => {}
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <View style={{ height: 600 }}>
+      <Agenda
+        items={{
+          '2020-05-22': [{ name: 'item 1 - any js object' }],
+          '2020-05-23': [{ name: 'item 2 - any js object', height: 20 }],
+          '2020-05-24': [],
+          '2020-05-25': [{ name: 'item 3 - any js object' }, { name: 'any js object' }],
+        }}
+        loadItemsForMonth={(month) => {
+          fetchCheckinForMonth(month.dateString)
+        }}
+        onDayPress={(day) => {
+          fetchCheckinForDay(day.dateString)
+        }}
+      />
     </View>
   )
 }
