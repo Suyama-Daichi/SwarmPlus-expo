c30341f17356f7b1564de2f9cb93f61d8877161d
daichi.suyama <symdit@gmail.com>
2021-03-17 10:54:33 +0900

    refactor: カレンダーの言語設定をルートに移動

--- screens/CheckinCalendar.tsx
+++ screens/CheckinCalendar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
-import { LocaleConfig, Agenda } from 'react-native-calendars'
+import { Agenda } from 'react-native-calendars'
 import { useDate } from '../hooks/useDate'
 
 export default function CheckinCalander() {
@@ -46,37 +46,3 @@ export default function CheckinCalander() {
     </View>
   )
 }
-
-LocaleConfig.locales.jp = {
-  monthNames: [
-    '1月',
-    '2月',
-    '3月',
-    '4月',
-    '5月',
-    '6月',
-    '7月',
-    '8月',
-    '9月',
-    '10月',
-    '11月',
-    '12月',
-  ],
-  monthNamesShort: [
-    '1月',
-    '2月',
-    '3月',
-    '4月',
-    '5月',
-    '6月',
-    '7月',
-    '8月',
-    '9月',
-    '10月',
-    '11月',
-    '12月',
-  ],
-  dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
-  dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
-}
-LocaleConfig.defaultLocale = 'jp'
