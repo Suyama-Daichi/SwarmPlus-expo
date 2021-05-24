848775b7f9e31a0daf11beceff09b9aa84519a1c
daichi.suyama <symdit@gmail.com>
2021-05-25 00:01:06 +0900

    fix: key周りの警告潰し

--- components/CheckinDetail.component.tsx
+++ components/CheckinDetail.component.tsx
@@ -11,15 +11,11 @@ import { useRecoil } from '../hooks/useRecoil'
 import { useFoursquare } from '../hooks/useFoursquare'
 import { CheckinsItem } from '../interface/Foursquare.type'
 import { commonStyles } from '../styles/styles'
-import Carousel, { Pagination } from 'react-native-snap-carousel'
-import SvgUri from 'react-native-svg-uri'
-
-const imageWidth = Dimensions.get('window').width * 1.0
+import { ImageCarousel } from '../components/carousel/ImageCarousel.component'
 
 export const CheckinDetail = ({ route }) => {
   const { item }: { item: CheckinsItem } = route.params
   const [checkinDetail, setCheckinDetail] = useState<CheckinsItem>()
-  const [activeSlide, setActiveSlide] = useState(0)
   const [images, setImages] = useState<string[]>([])
   const { fetchCheckinDetails } = useFoursquare()
   const { formatDistanceToNowForTimestamp, timestamp2Date } = useDate()
@@ -47,8 +43,10 @@ export const CheckinDetail = ({ route }) => {
           <View style={[commonStyles.rowCenter]}>
             <Text style={commonStyles.textSub}>
               いいね！:
-              {checkinDetail?.likes.groups[0].items.map((m) => (
-                <Text> {`${m.firstName ? m.firstName : ''}${m.lastName ? m.lastName : ''}`}</Text>
+              {checkinDetail?.likes.groups[0].items.map((m, i) => (
+                <Text key={i.toString()}>
+                  {`${m.firstName ? m.firstName : ''}${m.lastName ? m.lastName : ''}`}
+                </Text>
               ))}
             </Text>
           </View>
@@ -69,8 +67,11 @@ export const CheckinDetail = ({ route }) => {
           </View>
         )}
 
-        {checkinDetail?.comments.items?.map((comment) => (
-          <View style={[{ borderTopWidth: 0.3, borderColor: '#707070' }, commonStyles.rowCenter]}>
+        {checkinDetail?.comments.items?.map((comment, i) => (
+          <View
+            key={i.toString()}
+            style={[{ borderTopWidth: 0.3, borderColor: '#707070' }, commonStyles.rowCenter]}
+          >
             <Avatar
               rounded
               size={'medium'}
@@ -96,44 +97,7 @@ export const CheckinDetail = ({ route }) => {
     <View style={[commonStyles.bk_white]}>
       <FlatList
         keyExtractor={(_, _index) => _?.id + _index.toString()}
-        ListHeaderComponent={
-          <>
-            <Carousel
-              data={images}
-              renderItem={(d: any) => {
-                return (
-                  <Image
-                    source={
-                      d.item ? { uri: d.item } : require('../assets/images/20200501_noimage.png')
-                    }
-                    placeholderStyle={{ backgroundColor: colors.light.background }}
-                    transition
-                    style={{
-                      width: imageWidth,
-                      height: imageWidth,
-                    }}
-                    resizeMode="contain"
-                  />
-                )
-              }}
-              itemWidth={imageWidth}
-              sliderWidth={imageWidth}
-              onSnapToItem={(index: number) => setActiveSlide(index)} // for pagination
-            />
-            <Pagination
-              dotsLength={images.length} // dotの数
-              activeDotIndex={activeSlide} // どのdotをactiveにするか
-              containerStyle={{ paddingVertical: 9.19 }} // デフォルトではちと広い
-              dotStyle={{
-                width: 8,
-                height: 8,
-                borderRadius: 5,
-                marginHorizontal: 4,
-                backgroundColor: colors.light.pink,
-              }}
-            />
-          </>
-        }
+        ListHeaderComponent={ImageCarousel}
         data={[checkinDetail]}
         renderItem={itemRender}
       />
