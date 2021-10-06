af3a137363ff5056b9ea0f72102dd903c1169e87
daichi.suyama <symdit@gmail.com>
2021-10-06 22:48:41 +0900

    refactor: 型定義を修正

--- src/types/Foursquare.ts
+++ src/types/Foursquare.ts
@@ -11,7 +11,7 @@ export type Meta = {
 }
 
 export type Notification = {
-  type: string
+  type: 'notificationTray'
   item: NotificationItem
 }
 
@@ -20,21 +20,15 @@ export type NotificationItem = {
 }
 
 export type Response = {
-  checkins?: Checkins
+  checkins?: Summary<Checkin>
   checkin?: Checkin
   user?: User
 }
 
-export type Checkins = {
+export type Summary<T> = {
   count: number
-  items: Checkin[]
+  items: T[]
 }
-
-export type CheckinDetail = {
-  score: CheckinScore
-  user?: User
-  checkinShortUrl?: string
-} & Checkin
 
 export type Checkin = {
   id: string
@@ -47,57 +41,26 @@ export type Checkin = {
   likes: Likes
   like: boolean
   isMayor: boolean
-  photos: Photos
+  photos: PhotoSummary<Photo>
   posts: Posts
-  comments: Comments
+  comments: Summary<Comment>
   source: Source
   sticker?: Sticker
   with?: User[]
   visibility?: 'private'
 }
 
-export type Comments = {
-  count: number
-  items?: Comment[]
-}
+export type CheckinDetail = {
+  score: CheckinScore
+  user?: User
+  checkinShortUrl?: string
+} & Checkin
 
-export type Likes = {
-  count: number
-  groups: Users[]
-  summary?: string
-}
-
-export type Users = {
-  type: string
-  count: number
-  items: User[]
-}
-
-export type Icon = {
-  prefix: string
-  suffix: string
-}
-
-export type Photos = {
-  count: number
-  items: Photo[]
-  layout?: Layout
-}
-
-export type Photo = {
+export type Comment = {
   id: string
   createdAt: number
-  source: Source
-  width: number
-  height: number
-  demoted: boolean
   user: User
-  visibility: string
-} & Icon
-
-export type Source = {
-  name: string
-  url: string
+  text: string
 }
 
 export type User = {
@@ -113,11 +76,11 @@ export type User = {
   state?: string
   birthday?: number
   canonicalUrl?: string
-  checkins?: Checkins
+  checkins?: Summary<Checkin>
   bio?: string
   contact?: Contact
   createdAt?: number
-  friends?: Users
+  friends?: UserSummary<User>
 }
 
 export type Contact = {
@@ -127,21 +90,109 @@ export type Contact = {
   verifiedPhone: string
 }
 
-export type Layout = {
-  name: string
-  left?: Left
-  right?: Left
+export type Likes = {
+  count: number
+  groups: User[]
+  summary?: string
 }
 
-export type Left = {
+export type UserSummary<T> = {
+  type: string
+} & Summary<T>
+
+export type PhotoSummary<T> = {
+  layout?: Layout
+} & Summary<T>
+
+export type Photo = {
   id: string
+  createdAt: number
+  source: Source
+  width: number
+  height: number
+  demoted: boolean
+  user: User
+  visibility: string
+} & Icon
+
+export type Layout = {
+  name: string
+  left?: LeftRight
+  right?: LeftRight
+}
+export type LeftRight = {
+  id: string
+}
+
+export type Venue = {
+  id: string
+  name: string
+  location: Location
+  categories: Category[]
+  closed?: boolean
+  locked?: boolean
+  reasons?: Summary<Reason>
+}
+
+export type Category = {
+  id: string
+  name: string
+  pluralName: string
+  shortName: string
+  icon: Icon
+  primary: boolean
+}
+export type Icon = {
+  prefix: string
+  suffix: string
+}
+
+export type Source = {
+  name: string
+  url: string
 }
 
 export type Posts = {
   count: number
   textCount: number
 }
+export type Reason = {
+  summary: string
+  type: string
+  reasonName: string
+  target: Target<FluffyObject>
+}
 
+export type CheckinScore = {
+  total: number
+  scores: ScoreElement[]
+}
+
+export type ScoreElement = {
+  icon: string
+  message: string
+  points: number
+  target?: Target<PurpleObject>
+}
+
+export type Target<T> = {
+  type: string
+  object: T
+}
+
+export type PurpleObject = {
+  url: string
+}
+export type FluffyObject = {
+  id: string
+  type: string
+  target: ObjectTarget
+  ignorable: boolean
+}
+export type ObjectTarget = {
+  type: string
+  url: string
+}
 export type Sticker = {
   id: string
   name: string
@@ -155,42 +206,19 @@ export type Sticker = {
   points?: number
   bonusStatus?: string
 }
-
 export type StickerGroup = {
   name: string
   index: number
 }
-
+export type PickerPosition = {
+  page: number
+  index: number
+}
 export type Image = {
   prefix: string
   sizes: number[]
   name: string
 }
-
-export type PickerPosition = {
-  page: number
-  index: number
-}
-
-export type Venue = {
-  id: string
-  name: string
-  location: Location
-  categories: Category[]
-  closed?: boolean
-  locked?: boolean
-  reasons?: Reasons
-}
-
-export type Category = {
-  id: string
-  name: string
-  pluralName: string
-  shortName: string
-  icon: Icon
-  primary: boolean
-}
-
 export type Location = {
   address: string
   crossStreet?: string
@@ -205,66 +233,8 @@ export type Location = {
   country: string
   formattedAddress: string[]
 }
-
 export type LabeledLatLng = {
   label: string
   lat: number
   lng: number
-}
-
-export type Comment = {
-  id: string
-  createdAt: number
-  user: User
-  text: string
-}
-
-export type CheckinScore = {
-  total: number
-  scores: ScoreElement[]
-}
-
-export type ScoreElement = {
-  icon: string
-  message: string
-  points: number
-  target?: ScoreTarget
-}
-
-export type ScoreTarget = {
-  type: string
-  object: PurpleObject
-}
-
-export type PurpleObject = {
-  url: string
-}
-
-export type Reasons = {
-  count: number
-  items: ReasonsItem[]
-}
-
-export type ReasonsItem = {
-  summary: string
-  type: string
-  reasonName: string
-  target: ItemTarget
-}
-
-export type ItemTarget = {
-  type: string
-  object: FluffyObject
-}
-
-export type FluffyObject = {
-  id: string
-  type: string
-  target: ObjectTarget
-  ignorable: boolean
-}
-
-export type ObjectTarget = {
-  type: string
-  url: string
 }
