ff601a477db27326eb982e39a0e0485a0a8bbd19
daichi.suyama <symdit@gmail.com>
2021-11-13 00:02:03 +0900

    feat: マップボタン配置

--- src/components/pages/CheckinsByDay/CheckinsByDay.tsx
+++ src/components/pages/CheckinsByDay/CheckinsByDay.tsx
@@ -2,11 +2,14 @@ import { CheckinCard } from '@/components/card/CheckinCard'
 import { CheckinCalendarParamList } from '@/types'
 import { RouteProp, useRoute } from '@react-navigation/core'
 import React, { useEffect } from 'react'
-import { FlatList } from 'react-native'
+import { FlatList, View } from 'react-native'
 import { useCheckin } from '@/hooks/useCheckin'
 import { dateObj2Date, getDateString, getStartEndOfDay } from '@/service/dateFns'
 import { Checkin } from '@/types/Foursquare'
 import { useNavigation } from '@/hooks/useNavigation'
+import { Icon } from 'react-native-elements'
+import { COLORS } from '@/constants/Colors'
+import { useRecoil } from '@/hooks/useRecoil'
 
 const CheckinsByDay = () => {
   const { params } = useRoute<RouteProp<CheckinCalendarParamList, 'CheckinsByDay'>>()
@@ -14,12 +17,26 @@ const CheckinsByDay = () => {
   const navigation = useNavigation()
   const { checkins } = useCheckin()
   const { afterTimestamp, beforeTimestamp } = getStartEndOfDay(dateObject)
+  const date = dateObj2Date(dateObject)
   const displayCheckins = checkins.filter(
     (checkin) => afterTimestamp <= checkin.createdAt && checkin.createdAt < beforeTimestamp
   )
+  const { setSelectedDateOnMap } = useRecoil()
+
+  const onPressMapIcon = () => {
+    setSelectedDateOnMap(date)
+    navigation.navigate('MapNavigator')
+  }
 
   useEffect(() => {
-    navigation.setOptions({ headerTitle: getDateString(dateObj2Date(dateObject), 'yyyy/MM/dd(E)') })
+    navigation.setOptions({
+      headerTitle: getDateString(date, 'yyyy/MM/dd(E)'),
+      headerRight: () => (
+        <View style={{ marginHorizontal: 8 }}>
+          <Icon name={'map'} color={COLORS.common.primaryOrange} onPress={onPressMapIcon} />
+        </View>
+      ),
+    })
   }, [])
 
   const renderItem = ({ item }: { item: Checkin }) => {
