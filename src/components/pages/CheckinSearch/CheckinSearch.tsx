5c4ad6fbba872c99bf213c83fe2042dbf5c9d0b3
daichi.suyama <symdit@gmail.com>
2022-02-16 23:07:10 +0900

    feat: authがなければトップルートに遷移

--- src/components/pages/CheckinSearch/CheckinSearch.tsx
+++ src/components/pages/CheckinSearch/CheckinSearch.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import HeaderRight from '@/components/molecules/HeaderRight'
+import { useNavigation } from '@/hooks/useNavigation'
+import { useUser } from '@/hooks/useUser'
+import { generateImageUrl } from '@/service/utilFns'
+import React, { useEffect } from 'react'
+import { Avatar } from 'react-native-elements'
 
 const CheckinSearch = () => {
+  const { foursquareUser } = useUser()
+  const navigation = useNavigation()
+  useEffect(() => {
+    if (!foursquareUser) return
+
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderRight>
+          <Avatar
+            rounded
+            size={'small'}
+            source={{
+              uri: generateImageUrl(foursquareUser.photo, '50'),
+            }}
+            icon={{ name: 'person-outline' }}
+            onPress={() => navigation.push('UserProfile')}
+          />
+        </HeaderRight>
+      ),
+    })
+  }, [foursquareUser])
   return <></>
 }
 
--- src/hooks/useInitialize.ts
+++ src/hooks/useInitialize.ts
@@ -6,17 +6,23 @@ import { auth } from '@/service/firebase'
 import jwtDecode, { JwtPayload } from 'jwt-decode'
 import { FOURSQUARE_ACCESS_TOKEN } from '@/constants/StorageKeys'
 import storage from '@/service/reactNativeStorage'
+import { useNavigation } from '@/hooks/useNavigation'
 
 export const useInitialize = () => {
   const { fetchSetUser, setAuthUser } = useUser()
   const [loading, setLoading] = useState(true)
+  const navigation = useNavigation()
 
   useEffect(() => {
     // authを取得
     const unsubscribe = onAuthStateChanged(
       auth,
       (user) => {
-        if (!user) return
+        if (!user) {
+          // ログインしていなければトップに戻る
+          navigation.popToTop()
+          return
+        }
         const claims = jwtDecode<JwtPayload>(user.accessToken)
         storage.save({ key: FOURSQUARE_ACCESS_TOKEN, data: claims['accessToken'] })
         setAuthUser({ ...user })
