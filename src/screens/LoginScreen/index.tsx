569a54590212995738d393f97ee9b7cad17c50ae
daichi.suyama <symdit@gmail.com>
2022-05-17 22:33:41 +0900

    feat: ログイン機能を実装

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -3,10 +3,14 @@ import { FontAwesome5 } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 import { Button, Divider, Icon, Text } from 'native-base'
 import { useState } from 'react'
+import { useAuth } from '@/hooks/useAuth'
 
 export const LoginScreen = () => {
   const [visible, setVisible] = useState(false)
   const navigation = useNavigation()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { signInWithEmailHandler, loading } = useAuth()
 
   return (
     <VStack flex={1} space="10">
@@ -23,13 +27,14 @@ export const LoginScreen = () => {
       <VStack alignItems={'center'}>
         <VStack>
           <Text>メールアドレス</Text>
-          <Input placeholder="email" />
+          <Input placeholder="email" onChangeText={(email) => setEmail(email)} />
         </VStack>
         <VStack>
           <Text>パスワード</Text>
           <Input
             type={visible ? 'text' : 'password'}
             placeholder="password"
+            onChangeText={(password) => setPassword(password)}
             rightElement={
               <Icon
                 as={FontAwesome5}
@@ -42,7 +47,13 @@ export const LoginScreen = () => {
         </VStack>
       </VStack>
       <VStack alignItems={'center'}>
-        <Button width={'3/5'}>ログイン</Button>
+        <Button
+          isLoading={loading}
+          width={'3/5'}
+          onPress={() => signInWithEmailHandler(email, password)}
+        >
+          ログイン
+        </Button>
         <Button variant={'link'} onPress={() => navigation.push('signup')}>
           新規登録
         </Button>
