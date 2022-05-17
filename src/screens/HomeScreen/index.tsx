cc54ee5d38b3e665cd891ccb352e146eb7281ba9
daichi.suyama <symdit@gmail.com>
2022-05-17 23:33:30 +0900

    feat: ログアウト時の処理を実装

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,11 +1,18 @@
 import { View } from '@/components'
 import { signOut } from '@/services/auth'
+import { useNavigation } from '@react-navigation/native'
 import { Button } from 'native-base'
 
 export const HomeScreen = () => {
+  const navigation = useNavigation()
+  const logoutHandler = async () => {
+    await signOut()
+    navigation.push('login')
+  }
+
   return (
     <View>
-      <Button onPress={() => signOut()}>ログアウト</Button>
+      <Button onPress={logoutHandler}>ログアウト</Button>
     </View>
   )
 }
